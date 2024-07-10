b52d4a4f96a5072397025f06abfb2cb9e8aa9537
Reginald Addo <rnaddo@firstatlanticbank.com.gh>
2024-07-10 14:55:41 +0000

    Testing again

--- DeploymentTarget/Program.cs
+++ DeploymentTarget/Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chill good", "This is good", "this is good"
+    "Freezing", "Testing", "Chill good", "This is good", "this is good"
 };
 
 app.MapGet("/weatherforecast", () =>
